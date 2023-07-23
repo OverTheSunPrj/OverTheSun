97398f6167827bded430771ec02cfe549d8f0f1f
Sarehn <allen.deluca@atscom.it>
2023-07-23 20:10:18 +0200

    fixed Phaser integration

--- over-the-sun/src/App.tsx
+++ over-the-sun/src/App.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { appWindow } from "@tauri-apps/api/window";
 
-function App() {    
+function App() {  
+  const[size, setSize] = useState([1920,1080]);
+  
+  useEffect(() => {
+    appWindow.innerSize().then((innerSize) => {
+      setSize([innerSize.width, innerSize.height]);
+    });
+  });
   return (
-    <iframe src="src-phaser/overthesun.html" height="1080" width="1920"></iframe>
+    <iframe 
+      src={'src-phaser/overthesun.html?width='+ size[0] + '&height=' + size[1]}
+    ></iframe>
   );
 }
 
